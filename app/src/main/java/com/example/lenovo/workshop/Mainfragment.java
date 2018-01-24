d50fb262ccd04cdc9e20ad77a074696e1e11e537
Ayushi Jain <jainaayushii97@gmail.com>
2018-01-25 00:20:07 +0530

    Adding Fragment

---
+++ app/src/main/java/com/example/lenovo/workshop/Mainfragment.java
@@ -0,0 +1,44 @@
+package com.example.lenovo.workshop;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Mainfragment extends Fragment {
+
+   TabLayout tabLayout;
+    ViewPager viewPager;
+    public static final int tabcount = 2;
+    public static final String[] tabname = {
+            "Workshop available","Workshop joined"
+    };
+    Bundle b = new Bundle();
+
+    public Mainfragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.fragment_mainfragment,container,false);
+        return v;
+    }
+
+
+}
